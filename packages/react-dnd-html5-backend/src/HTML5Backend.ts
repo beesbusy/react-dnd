c09d1b4b2b0e8ad1a96a5e3df36f739d8a101b66
sdechamps <simon.dechamps@with.fr>
2020-02-05 10:56:33 +0100

    save

--- packages/react-dnd-html5-backend/src/HTML5Backend.ts
+++ packages/react-dnd-html5-backend/src/HTML5Backend.ts
@@ -7,7 +7,7 @@ import {
 	XYCoord,
 } from 'dnd-core'
 import EnterLeaveCounter from './EnterLeaveCounter'
-// import { isFirefox } from './BrowserDetector'
+import { isFirefox } from './BrowserDetector'
 import {
 	getNodeClientOffset,
 	getEventClientOffset,
@@ -513,20 +513,18 @@ export default class HTML5Backend implements Backend {
 
 		this.altKeyPressed = e.altKey
 
-		/**
-		 * This piece of code seems to create the visual glitch of dragged element position
-		 * because the coordinates returned by getEventClientOffset are wrong in this case
-		 */
+		if (!isFirefox() && e.fromElement !== null) {
+			// Don't emit hover in `dragenter` on Firefox due to an edge case.
+			// If the target changes position as the result of `dragenter`, Firefox
+			// will still happily dispatch `dragover` despite target being no longer
+			// there. The easy solution is to only fire `hover` in `dragover` on FF.
 
-		// if (!isFirefox()) {
-		// 	// Don't emit hover in `dragenter` on Firefox due to an edge case.
-		// 	// If the target changes position as the result of `dragenter`, Firefox
-		// 	// will still happily dispatch `dragover` despite target being no longer
-		// 	// there. The easy solution is to only fire `hover` in `dragover` on FF.
-		// 	this.actions.hover(dragEnterTargetIds, {
-		// 		clientOffset: getEventClientOffset(e),
-		// 	})
-		// }
+			// Hack: the first dragenter event, when starting to drag, returns wrong
+			// coordinates, so we silence it
+			this.actions.hover(dragEnterTargetIds, {
+				clientOffset: getEventClientOffset(e),
+			})
+		}
 
 		const canDrop = dragEnterTargetIds.some(targetId =>
 			this.monitor.canDropOnTarget(targetId),
